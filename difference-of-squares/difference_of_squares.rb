45288541a0f0d7e51fec6e46205a9c7bbe5dd2c4
gingin77 <ginniehench@gmail.com>
2017-12-07 19:38:51 -0500

    Use .sum instead of .reduce(:+)

--- difference-of-squares/difference_of_squares.rb
+++ difference-of-squares/difference_of_squares.rb
@@ -9,11 +9,11 @@ class Squares
   end
 
   def square_of_sum
-    @set.reduce(:+)**2
+    @set.sum**2
   end
 
   def sum_of_squares
-    @set.map { |n| n**2  }.reduce(:+)
+    @set.map { |n| n**2  }.sum
   end
 
   def difference
