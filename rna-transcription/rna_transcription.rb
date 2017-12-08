234685237ea270fb9d8de2b743aab4278156137a
gingin77 <ginniehench@gmail.com>
2017-12-07 23:23:50 -0500

    Use unless, all?, include? rather than if, any? !, include?
    The former syntax is more readable than the statement with NOT include because of the sequence.

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -12,7 +12,7 @@ class Complement
   }
 
   def self.of_dna(dna_seq)
-    return '' if dna_seq.chars.any? {|ltr| !DNA_TO_RNA.keys.include? ltr }
+    return '' unless dna_seq.chars.all? {|ltr| DNA_TO_RNA.keys.include? ltr}
     rna_seq = dna_seq.chars.map { |base| DNA_TO_RNA[base] }.join("")
   end
 end
