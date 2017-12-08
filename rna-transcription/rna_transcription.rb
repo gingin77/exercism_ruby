b4198b1ed862b47db6f0b5268ccedc4fa99bafbf
gingin77 <ginniehench@gmail.com>
2017-12-07 23:27:49 -0500

    Remove local var name that was not necessary

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -13,6 +13,6 @@ class Complement
 
   def self.of_dna(dna_seq)
     return '' unless dna_seq.chars.all? {|ltr| DNA_TO_RNA.keys.include? ltr}
-    rna_seq = dna_seq.chars.map { |base| DNA_TO_RNA[base] }.join("")
+    dna_seq.chars.map { |base| DNA_TO_RNA[base] }.join("")
   end
 end
