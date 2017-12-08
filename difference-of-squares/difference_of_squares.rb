5c5bb5e5195b3fe5fddaa0b67e42c858864d9170
gingin77 <ginniehench@gmail.com>
2017-12-07 19:43:06 -0500

    Use var name 'range' instead of 'set; place difference method earlier in sequence

--- difference-of-squares/difference_of_squares.rb
+++ difference-of-squares/difference_of_squares.rb
@@ -3,20 +3,19 @@ module BookKeeping
 end
 
 class Squares
-
   def initialize (number)
-    @set = 1..number
-  end
-
-  def square_of_sum
-    @set.sum**2
-  end
-
-  def sum_of_squares
-    @set.map { |n| n**2  }.sum
+    @range = 1..number
   end
 
   def difference
     square_of_sum - sum_of_squares
+  end
+
+  def square_of_sum
+    @range.sum**2
+  end
+
+  def sum_of_squares
+    @range.map { |n| n**2 }.sum
   end
 end
