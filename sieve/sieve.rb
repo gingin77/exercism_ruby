7bbaad6b7a2bcb0ef34433845a82dce3546272a5
gingin77 <ginniehench@gmail.com>
2017-12-13 19:30:50 -0500

    All sieve tests passed but code is clunky

--- sieve/sieve.rb
+++ sieve/sieve.rb
@@ -2,28 +2,9 @@ module BookKeeping
   VERSION = 1
 end
 
-=begin
-Create your range, starting at two and continuing up to and including the given limit. (i.e. [2, limit])
-
-The algorithm consists of repeating the following over and over:
-
-- take the next available unmarked number in your list (it is prime)
-- mark all the multiples of that number (they are not prime)
-
-Repeat until you have processed each number in your range.
-
-When the algorithm terminates, all the numbers in the list that have not
-been marked are prime.
-=end
-
 class Sieve
   def initialize(number)
-    if number > 1
-      @range = *2..number
-    else
-      @range = []
-    end
-    # puts @range
+    @range = *2..number
     @multiples = []
     @expected = []
     @modulo_num = @range[0]
@@ -31,24 +12,30 @@ class Sieve
 
   def sieve()
     @multiples = @range.select { |n| n % @modulo_num == 0 }
-    # puts @modulo_num
     @range = @range - @multiples
     @modulo_num = @range[0]
-    if !@range.empty?
+    unless @modulo_num.nil?
       @expected = @expected.push(@modulo_num)
     end
   end
 
   def primes
-    @expected = @expected.push(@modulo_num)
-    # puts @expected
+    unless @modulo_num.nil?
+      @expected = @expected.push(@modulo_num)
+    end
     while !@range.empty? do
       sieve()
     end
     @expected
   end
 end
-#
-# x = Sieve.new(1)
-#
-# x.primes
+
+
+x = Sieve.new(1)
+x.primes
+
+y = Sieve.new(2)
+y.primes
+
+q = Sieve.new(13)
+q.primes
