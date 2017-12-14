4398690dc69a2000a2e2192e53cb44071b4232ca
gingin77 <ginniehench@gmail.com>
2017-12-13 19:57:22 -0500

    Sieve exerism first submission version

--- sieve/sieve.rb
+++ sieve/sieve.rb
@@ -5,34 +5,17 @@ end
 class Sieve
   def initialize(number)
     @range = *2..number
-    @multiples = []
     @expected = []
-    @modulo_num = @range[0]
-  end
-
-  def sieve()
-    unless @modulo_num.nil?
-      @expected = @expected.push(@modulo_num)
-    end
-    @multiples = @range.select { |n| n % @modulo_num == 0 }
-    @range = @range - @multiples
-    @modulo_num = @range[0]
   end
 
   def primes
-    while !@range.empty? do
-      sieve()
-    end
+    sieve() until @range.empty?
     @expected
   end
+
+  def sieve()
+    modulo_num = @range[0]
+    @expected = @expected.push(modulo_num) unless modulo_num.nil?
+    @range = @range - @range.select { |n| n % modulo_num == 0 }
+  end
 end
-
-
-x = Sieve.new(1)
-x.primes
-
-y = Sieve.new(2)
-y.primes
-
-q = Sieve.new(13)
-q.primes
