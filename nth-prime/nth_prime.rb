45c790b013d6f94a08a89f62c192c172e3a68c62
gingin77 <ginniehench@gmail.com>
2018-01-06 19:29:57 -0500

    last nth-prime answer submitted to exercism

--- nth-prime/nth_prime.rb
+++ nth-prime/nth_prime.rb
@@ -3,36 +3,32 @@ module BookKeeping
 end
 
 class Prime
+  @primes ||= [2, 3]
+
   def self.nth(n)
     raise ArgumentError if n.zero?
     @n = n
-    @primes ||= gen_primes
-    @n >= 500 ? primes_over_500 : @primes[@n - 1]
+    more_primes[@n - 1]
   end
 
-  private
-
-  def self.gen_primes
-    obj = *2...3571
-    obj.lazy.inject(obj) do |result, el|
-      result.reject { |i| i != el && i % el == 0 }
-    end
-  end
-
-  def self.primes_over_500
-    gen_primes_over_500[@n - 1]
-  end
-
-  def self.gen_primes_over_500
-    can = @primes.last + 2
+  def self.more_primes
+    candidate = @primes.last + 2
     while @primes.size < @n
-      lmt = Math.sqrt(can).ceil
-      factors = *@primes.first..lmt
-      if factors.all? { |fact| can % fact != 0 }
-        @primes << can
+      if is_prime?(candidate)
+        @primes << candidate
       end
-      can += 2
+      candidate += 2
     end
     @primes
   end
+
+  def self.is_prime?(candidate)
+    sqrt = Math.sqrt(candidate)
+    @primes.all? do |divisor|
+      return true if divisor > sqrt
+      candidate % divisor != 0
+    end
+  end
+
+  private_class_method :is_prime?, :more_primes
 end
