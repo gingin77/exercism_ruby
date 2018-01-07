230675088944d68b83a4a6f3d06ec2824546e9dc
gingin77 <ginniehench@gmail.com>
2018-01-06 19:30:46 -0500

    Revisit hamming solution

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,10 +1,8 @@
 class Hamming
-    def self.compute(top_strand, bottom_strand)
-      raise ArgumentError if top_strand.length != bottom_strand.length
-      top_strand.chars.zip(bottom_strand.chars).collect{
-        |x, y| x == y
-      }.count(false)
-    end
+  def self.compute(top_strand, bottom_strand)
+    raise ArgumentError, "The strand lengths do not match" if top_strand.length != bottom_strand.length
+    top_strand.chars.zip(bottom_strand.chars).count{|x, y| x != y}
+  end
 end
 
 module BookKeeping
