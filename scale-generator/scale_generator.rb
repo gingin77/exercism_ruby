a2544fd5cc07c5b8c9cfec561f2b7d69c76c8f88
gingin77 <ginniehench@gmail.com>
2018-01-27 18:32:39 -0500

    More refactoring:
    Simplify the private choose_notes method and convert it to choose_notes_and_rotate; move rotate method out of pitches; initialize @notes instance variable
    Introduce STEP_HASH variable to replace if/elsif/else chain in pitches

--- scale-generator/scale_generator.rb
+++ scale-generator/scale_generator.rb
@@ -5,11 +5,15 @@ end
 class Scale
   NOTES_SHARP = %w(A A# B C C# D D# E F F# G G#)
   NOTES_FLAT = %w(A Bb B C Db D Eb E F Gb G Ab)
+  
+  TONICS_THAT_USE_FLATS = %w(F Bb Eb Ab Db Gb d g c f bb eb)
+  STEP_HASH = { "m" => 1, "M" => 2, "A" => 3}
 
   def initialize(tonic, scale, interval = 'mmmmmmmmmmmm')
     @tonic = tonic
     @scale = scale
     @interval = interval
+    @notes = choose_notes_and_rotate
   end
 
   def name
@@ -17,34 +21,20 @@ class Scale
   end
 
   def pitches
-    notes = choose_notes
-    notes = notes.rotate(notes.index(@tonic.capitalize))
-
     [].tap do |pitches|
+      pitches << @notes[0]
       index_pos = 0
-      pitches << notes[0]
       @interval.chars[0..-2].each do |c|
-        if c == "m"
-          index_pos += 1
-        elsif c == "M"
-          index_pos += 2
-        else
-          index_pos += 3
-        end
-        pitches << notes[index_pos]
+        index_pos += STEP_HASH[c]
+        pitches << @notes[index_pos]
       end
     end
   end
 
   private
 
-  def choose_notes
-    if [:chromatic, :minor, :major].include? @scale
-      (%w(G D A E B F# C e b f# c# g# d#).include? @tonic) ? NOTES_SHARP : NOTES_FLAT
-    elsif [:dorian, :lydian, :phrygian, :octatonic, :pentatonic, :enigma,].include? @scale
-      NOTES_SHARP
-    else
-      NOTES_FLAT
-    end
+  def choose_notes_and_rotate
+    notes = (TONICS_THAT_USE_FLATS.include? @tonic) ? NOTES_FLAT : NOTES_SHARP
+    notes = notes.rotate(notes.index(@tonic.capitalize))
   end
 end
